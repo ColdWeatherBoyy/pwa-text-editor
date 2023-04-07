b92db54ab81845e46aabf28093e6aaf66b06c97a
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-04-07 16:44:32 -0400

    fixes install button disappearing

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -5,7 +5,6 @@ const butInstall = document.getElementById("buttonInstall");
 window.addEventListener("beforeinstallprompt", (event) => {
 	event.preventDefault();
 	window.deferredPrompt = event;
-	butInstall.classList.toggle("hidden", false);
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
@@ -19,12 +18,10 @@ butInstall.addEventListener("click", async () => {
 	promptEvent.prompt();
 
 	window.deferredPrompt = null;
-
-	butInstall.classList.toggle("hidden", true);
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener("appinstalled", (event) => {
 	window.deferredPrompt = null;
-	butInstall.classList.toggle("hidden", true);
+	butInstall.setAttribute("style", "display: none;");
 });
